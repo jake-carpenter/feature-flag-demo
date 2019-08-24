303bc886a794353784d83d11f94f03b22f301f3a
Jake Carpenter <jake@jakecarpenter.dev>
2019-08-23 22:45:43 -0600

    Initial commit

---
+++ FeatureFlagDemo/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FeatureFlagDemo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "hello world";
+        }
+    }
+}
